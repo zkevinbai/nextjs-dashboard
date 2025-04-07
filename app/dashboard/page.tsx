10a0a82ebbdd8ae08a2a559041575ba544536a74
Kevin Bai <lkevin957@gmail.com>
2025-04-06 22:52:58 -0700

    layout

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,13 @@
+import { lusitana } from '../ui/fonts';
+
+export default function Page() {
+  return (
+    <div>
+      <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
+        Dashboard
+      </h1>
+
+      hello world 
+    </div>
+  );
+}
